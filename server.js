5bb7b053990cc7da0670f9b4b90bb6a6ca861312
rxsil <155285668+rxsil@users.noreply.github.com>
2024-04-12 23:10:11 +0000

    - 15 - Custom error handler

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import {
+  errorResponserHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler";
+
+// Routes
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 connectDB();
@@ -10,5 +17,12 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
+
+app.use("/api/users", userRoutes);
+
+app.use(invalidPathHandler);
+app.use(errorResponserHandler);
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
