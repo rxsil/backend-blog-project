2c54a55b3ac0d43a096a1b9c5424f6fd3ec7f1c9
rxsil <155285668+rxsil@users.noreply.github.com>
2024-04-14 10:49:47 +0000

    - 17 - update user's profile info & picture

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,23 +1,25 @@
+import { uploadPicture } from "../middleware/uploadPictureMiddleware";
 import User from "../models/User";
+import { fileRemover } from "../utils/fileRemover";
 
-export const registerUser = async (req, res, next) => {
+const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
-   
-    // check whether the user exists or not
-    let user = await User.findOne({ email });
 
+ // check whether the user exists or not
+    let user = await User.findOne({ email });
+   
     if (user) {
       throw new Error("User have already registered");
     }
-
-     // creating a new user
-     user = await User.create({
+    
+    // creating a new user
+    user = await User.create({
       name,
       email,
       password,
     });
-
+    
     return res.status(201).json({
       _id: user._id,
       avatar: user.avatar,
@@ -32,16 +34,16 @@ export const registerUser = async (req, res, next) => {
   }
 };
 
-export const loginUser = async (req, res, next) => {
+const loginUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-
+    
     let user = await User.findOne({ email });
 
     if (!user) {
       throw new Error("Email not found");
     }
-
+    
     if (await user.comparePassword(password)) {
       return res.status(201).json({
         _id: user._id,
@@ -60,10 +62,10 @@ export const loginUser = async (req, res, next) => {
   }
 };
 
-export const userProfile = async (req, res, next) => {
+const userProfile = async (req, res, next) => {
   try {
     let user = await User.findById(req.user._id);
-
+  
     if (user) {
       return res.status(201).json({
         _id: user._id,
@@ -83,4 +85,93 @@ export const userProfile = async (req, res, next) => {
   }
 };
 
-export { registerUser, loginUser };
+const updateProfile = async (req, res, next) => {
+  try {
+    let user = await User.findById(req.user._id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password && req.body.password.length < 6) {
+      throw new Error("Password length must be at least 6 character");
+    } else if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUserProfile = await user.save();
+
+    res.json({
+      _id: updatedUserProfile._id,
+      avatar: updatedUserProfile.avatar,
+      name: updatedUserProfile.name,
+      email: updatedUserProfile.email,
+      verified: updatedUserProfile.verified,
+      admin: updatedUserProfile.admin,
+      token: await updatedUserProfile.generateJWT(),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateProfilePicture = async (req, res, next) => {
+  try {
+    const upload = uploadPicture.single("profilePicture");
+
+    upload(req, res, async function (err) {
+      if (err) {
+        const error = new Error("An unknown error occured when uploading " + err.message);
+        next(error);
+      } else {
+        // every thing went well
+        if (req.file) {
+          const updatedUser = await User.findByIdAndUpdate(
+            req.user._id,
+            {
+              avatar: req.file.filename,
+            },
+            { new: true }
+          );
+          res.json({
+            _id: updatedUser._id,
+            avatar: updatedUser.avatar,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            verified: updatedUser.verified,
+            admin: updatedUser.admin,
+            token: await updatedUser.generateJWT(),
+          });
+        } else {
+          let filename;
+          let updatedUser = await User.findById(req.user._id);
+          filename = updatedUser.avatar;
+          updatedUser.avatar = "";
+          await updatedUser.save();
+          fileRemover(filename);
+          res.json({
+            _id: updatedUser._id,
+            avatar: updatedUser.avatar,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            verified: updatedUser.verified,
+            admin: updatedUser.admin,
+            token: await updatedUser.generateJWT(),
+          });
+        }
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export {
+  registerUser,
+  loginUser,
+  userProfile,
+  updateProfile,
+  updateProfilePicture,
+};
